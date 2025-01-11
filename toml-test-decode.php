5ae83ed4edf3747658e65d2ac8d3dd6c4d7723ce
Vano Devium <webdevium@gmail.com>
2025-01-11 13:20:21 +0200

    v1.0.5

--- toml-test-decode.php
+++ toml-test-decode.php
@@ -10,6 +10,5 @@ try {
     echo json_encode(TomlTag::tagObject(toml_decode(file_get_contents('php://stdin'))));
     exit(0);
 } catch (Throwable $e) {
-    //exit($e->getMessage()."\n".$e->getTraceAsString());
     exit(1);
 }
--- toml-test-encode.php
+++ toml-test-encode.php
@@ -10,6 +10,5 @@ try {
     echo toml_encode(TomlTag::untagObject(json_decode(file_get_contents('php://stdin'))));
     exit(0);
 } catch (Throwable $e) {
-    //exit($e->getMessage()."\n".$e->getTraceAsString());
     exit(1);
 }
