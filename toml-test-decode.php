ff5319e998c803567698ee386ac9bce43051410f
Vano Devium <webdevium@gmail.com>
2025-01-11 13:22:49 +0200

    v1.0.5

--- toml-test-decode.php
+++ toml-test-decode.php
@@ -7,7 +7,7 @@ use Devium\Toml\TomlTag;
 include_once './vendor/autoload.php';
 
 try {
-    echo json_encode(TomlTag::tagObject(toml_decode(file_get_contents('php://stdin'))));
+    echo json_encode(TomlTag::tagObject(toml_decode(file_get_contents('php://stdin'))), JSON_THROW_ON_ERROR);
     exit(0);
 } catch (Throwable $e) {
     exit(1);
--- toml-test-encode.php
+++ toml-test-encode.php
@@ -7,7 +7,7 @@ use Devium\Toml\TomlTag;
 include_once './vendor/autoload.php';
 
 try {
-    echo toml_encode(TomlTag::untagObject(json_decode(file_get_contents('php://stdin'))));
+    echo toml_encode(TomlTag::untagObject(json_decode(file_get_contents('php://stdin'), false, 512, JSON_THROW_ON_ERROR)));
     exit(0);
 } catch (Throwable $e) {
     exit(1);
